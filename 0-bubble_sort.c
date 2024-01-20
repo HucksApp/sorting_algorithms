5abc466e0baf5497aa3e3785bf95729015bc6f9f
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-01-20 01:06:09 -0800

    :construction:

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,8 +4,7 @@
  * bubble_sort - sorts an array of integers in ascending orde
  * @array: the array to be sort
  * @size: the size of the array
- *		
- 		Big O Details
+ * Big O Details
  *  fn(x) E 0(n2)
  *  Best Case: O(n): check all with list already
  *      in asscending order(no bigger num comes first)
@@ -16,19 +15,17 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-    int index, endi, temp;
+  int index, endi, temp;
 
-
-    for (endi = size - 1 ; endi; endi--)
-    {
-        for (index = 0; index < endi ;  index++)
-            if (array[index] > array[index + 1])
-            {
-                temp = array[index];
-                array[index] = array[index + 1];
-                array[index + 1] = temp;
-                print_array(array, size);
-            }       
-            
-    }
+  for (endi = size - 1 ; endi; endi--)
+  {
+   for (index = 0; index < endi ;  index++)
+     if (array[index] > array[index + 1])
+     {
+       temp = array[index];
+       array[index] = array[index + 1];
+       array[index + 1] = temp;
+       print_array(array, size);
+     }
+  }
 }
