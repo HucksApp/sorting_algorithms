62b33dfa56ed047bcad321a26e8aecc493d12e71
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-01-21 19:24:15 -0800

    :construction:

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -5,39 +5,38 @@
  * @list: doubly liked list
  *
  */
- void insertion_sort_list(listint_t **list)
+void insertion_sort_list(listint_t **list)
 {
 	listint_t *sot_tail, *curr, *nxt = *list;
 
-	if(!list || !nxt || !(nxt->next))
+	if (!list || !nxt || !(nxt->next))
 		return;
 
-	for(; nxt; nxt = nxt->next)
+	for (; nxt; nxt = nxt->next)
 	{
 		curr = nxt;
 		sot_tail = nxt->prev;
 
 
-		while(sot_tail)
+		while (sot_tail)
 		{
-			if(sot_tail->n > curr->n)
+			if (sot_tail->n > curr->n)
 			{
-				if(curr->next)
+				if (curr->next)
 					 curr->next->prev = sot_tail;
-				if(sot_tail->prev)
+				if (sot_tail->prev)
 					sot_tail->prev->next = curr;
-					
+
 				sot_tail->next = curr->next;
 				curr->prev = sot_tail->prev;
 				curr->next = sot_tail;
 				sot_tail->prev = curr;
-	
-				if(sot_tail == *list)
+
+				if (sot_tail == *list)
 					*list = curr;
 				print_list((const listint_t *)*list);
 			}
 			sot_tail = sot_tail->prev;
 		}
-		
 	}
 }
