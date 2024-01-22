f092faa831aa15157cf366e8f296b43798d84566
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-01-21 19:18:23 -0800

    :construction

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,25 +7,37 @@
  */
  void insertion_sort_list(listint_t **list)
 {
-	listint_t *head, *prev;
-	int value;
+	listint_t *sot_tail, *curr, *nxt = *list;
 
-	if (list == NULL || (*list)->next == NULL || (*list) == NULL)
-	{
+	if(!list || !nxt || !(nxt->next))
 		return;
-	}
-	head = *list;
-	while (head)
-	{
-		prev = head->prev;
-				value = head->n;
 
-		while (prev && prev->n > value)
+	for(; nxt; nxt = nxt->next)
+	{
+		curr = nxt;
+		sot_tail = nxt->prev;
+
+
+		while(sot_tail)
 		{
-			swap(prev, head, list);
-			print_list(*list);
-			prev = head->prev;
+			if(sot_tail->n > curr->n)
+			{
+				if(curr->next)
+					 curr->next->prev = sot_tail;
+				if(sot_tail->prev)
+					sot_tail->prev->next = curr;
+					
+				sot_tail->next = curr->next;
+				curr->prev = sot_tail->prev;
+				curr->next = sot_tail;
+				sot_tail->prev = curr;
+	
+				if(sot_tail == *list)
+					*list = curr;
+				print_list((const listint_t *)*list);
+			}
+			sot_tail = sot_tail->prev;
 		}
-		head = head->next;
+		
 	}
 }
