8b2e8a2adbe9c2464a087d1a3d5604999c654788
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-01-23 02:56:36 -0800

    :construction:

---
+++ 2-selection_sort.c
@@ -0,0 +1,25 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Implementation of selection Sort Algrithme
+ * @array: Array to sort type int *
+ * @size: The Size of The Given Array
+ *
+ * Return: (Void) Sorted Array
+ */
+void selection_sort(int *array, size_t size)
+{
+	int index, iter, ind, smallest;
+
+
+	for(index = 0; index < size-1 ; index++)
+	{
+    smallest = array[index];
+    for(iter = index + 1 ; iter < size; iter++)
+      if(array[iter] < smallest)
+        smallest = array[iter], ind = iter;
+    array[ind] = array[index];
+    array[index] = smallest;
+    print_array(array, size);
+	}
+}
