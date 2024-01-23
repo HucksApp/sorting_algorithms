54e9e4d0276212e5b572a448946939cca52b2e0d
Ahrabprince <58187974+HucksApp@users.noreply.github.com>
2024-01-23 03:56:56 -0800

    :construction:

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -15,12 +15,13 @@ void selection_sort(int *array, size_t size)
 	for (index = 0; index < (int)size - 1 ; index++)
 	{
 		smallest = array[index];
-		for (iter = index + 1 ; iter < (int)size; iter++)
-			if (array[iter] < smallest)
-				smallest = array[iter], ind = iter;
-
-		array[ind] = array[index];
-		array[index] = smallest;
-		print_array(array, size);
+		if (smallest != -1)
+			for (iter = index + 1 ; iter < (int)size; iter++)
+				if (array[iter] < smallest)
+					smallest = array[iter], ind = iter;
+	
+			array[ind] = array[index];
+			array[index] = smallest;
+			print_array(array, size);
 	}
 }
